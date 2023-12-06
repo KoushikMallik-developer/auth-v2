d75e3348657e6dce45b8c7beae871d9641eaed70
Koushik Mallik <koushikmallik001@gmail.com>
2023-12-06 23:44:35 +0530

    cleaned up.

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -14,8 +14,7 @@ def user_list():
             "lname": "Google",
             "dob": None,
             "phone": None,
-            "password": "b'gAAAAABlcLJF0FLjcCWFUWQfRl442eAlZ9_IGgfUJAHlXpinOI_YrnpfUtXBfKpJifVI9T9JNuSUy9ax3oCyLbbqouA8rjd9Lg=='",
-            # pragma: allowlist-secret
+            "password": "b'gAAAAABlcLJF0FLjcCWFUWQfRl442eAlZ9_IGgfUJAHlXpinOI_YrnpfUtXBfKpJifVI9T9JNuSUy9ax3oCyLbbqouA8rjd9Lg=='",  # pragma: allowlist-secret
             "image": "/images/users/defaultUserImage.png",
             "is_active": True,
             "account_type": "Regular",
@@ -26,8 +25,7 @@ def user_list():
             "email": "animeshece1998@gmail.com",
             "fname": "Koushik",
             "lname": "Google",
-            "password": "b'gAAAAABlcLJF0FLjcCWFUWQfRl442eAlZ9_IGgfUJAHlXpinOI_YrnpfUtXBfKpJifVI9T9JNuSUy9ax3oCyLbbqouA8rjd9Lg=='",
-            # pragma: allowlist-secret
+            "password": "b'gAAAAABlcLJF0FLjcCWFUWQfRl442eAlZ9_IGgfUJAHlXpinOI_YrnpfUtXBfKpJifVI9T9JNuSUy9ax3oCyLbbqouA8rjd9Lg=='",  # pragma: allowlist-secret
             "dob": None,
             "phone": None,
             "image": "/images/users/defaultUserImage.png",
