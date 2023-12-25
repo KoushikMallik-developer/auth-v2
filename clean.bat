516cfb02574a807543184c623aab990dcb0a89df
GoKU <koushikmallik001@gmail.com>
2023-12-25 12:03:14 +0530

    updated clean.bat file.

--- clean.bat
+++ clean.bat
@@ -2,6 +2,12 @@
 
 rem This batch file will run flake8 and black on all Python files in the current directory.
 
+rem update dependencies
+pip freeze > ./dependencies/dev-requirements.txt
+pip freeze > ./dependencies/qa-requirements.txt
+pip freeze > ./dependencies/staging-requirements.txt
+pip freeze > ./dependencies/prod-requirements.txt
+
 rem Run flake8.
 flake8 .
 
